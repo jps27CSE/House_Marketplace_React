387c70da36f6ddf2be25914cc92fe680d71793c4
jps27CSE <58485174+jps27CSE@users.noreply.github.com>
2022-08-16 23:37:49 +0600

    save user to firestore

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -7,6 +7,7 @@ import {
   createUserWithEmailAndPassword,
   updateProfile,
 } from "firebase/auth";
+import { doc, setDoc, serverTimestamp } from "firebase/firestore";
 
 import { db } from "../firebase.config";
 
@@ -44,6 +45,13 @@ const SignUp = () => {
       updateProfile(auth.currentUser, {
         displayName: name,
       });
+
+      const formDataCopy = { ...formData };
+      delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
+
+      await setDoc(doc(db, "users", user.uid), formDataCopy);
+
       navigate("/");
     } catch (error) {
       console.log(error);

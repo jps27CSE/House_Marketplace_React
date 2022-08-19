b707b2abb26412f83fb1f719a01058fff9e82ead
jps27CSE <58485174+jps27CSE@users.noreply.github.com>
2022-08-19 13:17:43 +0600

    display update data and edit

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,12 @@
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
+import { updateDoc } from "firebase/firestore";
+import { db } from "../firebase.config";
 
 const Profile = () => {
   const auth = getAuth();
+  const [changeDetails, setChangeDetails] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -17,6 +20,17 @@ const Profile = () => {
     navigate("/");
   };
 
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = () => {
+    console.log(123);
+  };
+
   return (
     <div className="profile">
       <header className="profileHeader">
@@ -25,6 +39,32 @@ const Profile = () => {
           Logout
         </button>
       </header>
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changeDetails && onSubmit();
+              setChangeDetails((preveState) => !preveState);
+            }}
+          >
+            {changeDetails ? "done" : "change"}
+          </p>
+        </div>
+        <div className="profileCard">
+          <form>
+            <input
+              type="text"
+              id="name"
+              className={!changeDetails ? "profileName" : "profileNameActive"}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChange}
+            />
+          </form>
+        </div>
+      </main>
     </div>
   );
 };

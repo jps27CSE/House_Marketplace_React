0120096ae2129bb9a598985286ce16456a47e987
jps27CSE <58485174+jps27CSE@users.noreply.github.com>
2022-08-20 23:47:49 +0600

    google auth

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -11,6 +11,7 @@ import {
 import { doc, setDoc, serverTimestamp } from "firebase/firestore";
 
 import { db } from "../firebase.config";
+import OAuth from "../components/OAuth";
 
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -111,6 +112,7 @@ const SignUp = () => {
             </button>
           </div>
         </form>
+        <OAuth />
         <Link to="/sign-in" className="registerLink">
           Sign In Instead
         </Link>
